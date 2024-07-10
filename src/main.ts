b19c16d3656aa9abbc49d3efb0b802f3a8cbaff4
firesquid6 <jdeiss06@gmail.com>
2024-07-09 23:37:20 -0500

    add the root page

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,14 @@
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-  </div>
-`
+import { inkit } from "./inkit"
+import { Root } from "./pages/root"
+
+const app = document.querySelector<HTMLDivElement>('#app')
+
+
+if (!app) {
+  // something has gone horribly wrong
+  throw new Error("Could not find #app. You screwed up big time.")
+}
+
+
+inkit.start(app)
+app.innerHTML = Root().toString()
---
+++ src/pages/root.tsx
@@ -0,0 +1,8 @@
+export function Root() {
+  return (
+    <div>
+      <h1>Welcome to my app!</h1>
+    </div>
+  )
+
+}
